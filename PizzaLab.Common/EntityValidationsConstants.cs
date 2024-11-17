19efeb1a24005a323beb2af49ef7d579eabe0826
AleksandarNiko <145496882+AleksandarNiko@users.noreply.github.com>
2024-11-17 11:39:31 +0200

    Created application constants.

---
+++ PizzaLab.Common/EntityValidationsConstants.cs
@@ -0,0 +1,51 @@
+﻿namespace PizzaLab.Common
+{
+    public static class EntityValidationsConstants
+    {
+        public static class Pizza
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 30;
+
+            public const int ImageMinLength = 5;
+            public const int ImageMaxLength = 512;
+
+            public const int DescriptionMinLength = 5;
+            public const int DescriptionMaxLength = 1024;
+
+        }
+
+        public static class Product
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 30;
+        }
+
+        public static class Dough
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 30;
+        }
+
+        public static class Topping
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 30;
+        }
+
+        public static class Menu
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 30;
+
+            public const int DescriptionMinLength = 3;
+            public const int DescriptionMaxLength = 255;
+        }
+
+        public static class Admin
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 30;
+        }
+    }
+}
---
+++ PizzaLab.Common/GeneralApplicationConstants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaLab.Common
+{
+    public static  class GeneralApplicationConstants
+    {
+        public const int ReleaseYear = 2023;
+
+        public const string AdminAreaName = "Admin";
+        public const string AdminRoleName = "Administrator";
+        public const string DevelopmentAdminEmail = "admin@houserenting.bg";
+
+
+        public const string OnlineUsersCookieName = "IsOnline";
+        public const int LastActivityBeforeOfflineMinutes = 10;
+    }
+}
