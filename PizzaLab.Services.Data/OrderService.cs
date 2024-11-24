e308778c4834cb0d1863e2a93fb748286cb4296f
AleksandarNiko <145496882+AleksandarNiko@users.noreply.github.com>
2024-11-24 19:42:45 +0200

    Added functionality for OrderService

---
+++ PizzaLab.Services.Data/OrderService.cs
@@ -0,0 +1,60 @@
+﻿namespace PizzaLab.Services.Data 
+{ 
+
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+
+    using PizzaLab.Data;
+    using PizzaLab.Data.Models;
+    using PizzaLab.Services.Data.Interfaces;
+
+    public class OrderService : IOrderService
+    {
+        private readonly PizzaLabDbContext dbContext;
+        private readonly ICartService cartService;
+
+        public OrderService(PizzaLabDbContext _dbContext, ICartService _cartService)
+        {
+            this.dbContext = _dbContext;
+            this.cartService = _cartService;
+        }
+
+        public async Task AddOrderAsync(string userId)
+        {
+            decimal price = await cartService.GetFinalPrizeAsync(userId);
+
+            Order order = new Order()
+            {
+                OrderDate = DateTime.Now,
+                UserId = Guid.Parse(userId),
+                Price = price
+            };
+
+            await dbContext.Orders.AddAsync(order);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task EmptyCartAsync(string userId)
+        {
+            List<Cart> carts = await dbContext
+                .Carts
+                .Where(c => c.UserId == Guid.Parse(userId) )
+                .ToListAsync();
+
+            dbContext.Carts.RemoveRange(carts);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task RemoveCartPizzasAsync(string userId)
+        {
+            List<CartPizza> cartPizzas = await dbContext
+                .CartsPizzas
+                .Where(cp => cp.UserId == Guid.Parse(userId))
+                .ToListAsync();
+
+            dbContext.CartsPizzas.RemoveRange(cartPizzas);
+            await dbContext.SaveChangesAsync();
+                
+        }
+    }
+}
