544261114805c631e816d539497135765b95e080
AleksandarNiko <145496882+AleksandarNiko@users.noreply.github.com>
2024-11-25 07:18:41 +0200

    Fixed User service

---
+++ PizzaLab.Services.Data/UserService.cs
@@ -0,0 +1,35 @@
+﻿namespace PizzaLab.Services.Data
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+
+    using PizzaLab.Data;
+    using PizzaLab.Services.Data.Interfaces;
+    using PizzaLab.Web.ViewModels.User;
+
+    public class UserService : IUserService
+    {
+        private readonly PizzaLabDbContext dbContext;
+
+        public UserService(PizzaLabDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<UserViewModel>> AllAsync()
+        {
+           
+        List<UserViewModel> allUsers = await this.dbContext
+                   .Users
+                   .Select(u => new UserViewModel()
+                   {
+                       Id = u.Id.ToString(),
+                       Email = u.Email,
+                   })
+                   .ToListAsync();
+
+                return allUsers;
+        }
+    }
+}
