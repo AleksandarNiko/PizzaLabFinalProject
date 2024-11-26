050f7e8d0580d19c5dcd1ea8ae751220d040ffeb
AleksandarNiko <145496882+AleksandarNiko@users.noreply.github.com>
2024-11-26 22:31:56 +0200

    Added statistic ViewModel  for Home/Index

---
+++ PizzaLab.Services.Data.Models/StatisticsServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PizzaLab.Services.Data.Models
+{
+    public class StatisticsServiceModel
+    {
+        public int TotalMenus { get; set; }
+
+        public int TotalPizzas { get; set; }
+    }
+}
