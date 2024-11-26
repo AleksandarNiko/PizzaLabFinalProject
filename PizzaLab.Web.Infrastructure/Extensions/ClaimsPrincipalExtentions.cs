a045af74ee18de470a21e094998db27c03bbbfef
AleksandarNiko <145496882+AleksandarNiko@users.noreply.github.com>
2024-11-26 22:49:03 +0200

    Code cleanup

---
+++ PizzaLab.Web.Infrastructure/Extensions/ClaimsPrincipalExtentions.cs
@@ -0,0 +1,18 @@
+﻿namespace PizzaLab.Web.Infrastructures.Extentions
+{
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using System.Security.Claims;
+
+    using static PizzaLab.Common.GeneralApplicationConstants;
+    public static class ClaimsPrincipalExtentions
+    {
+        public static string? GetId(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+        public static bool IsAdmin(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(AdminRoleName);
+        }
+    }
+}
