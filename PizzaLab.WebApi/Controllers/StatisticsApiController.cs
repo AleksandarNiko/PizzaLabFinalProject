efa17c7faee2ed5dbf23708eed4b03c7c2baf5c2
AleksandarNiko <145496882+AleksandarNiko@users.noreply.github.com>
2024-12-11 21:23:27 +0200

    Added statistics

---
+++ PizzaLab.WebApi/Controllers/StatisticsApiController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaLab.Services.Data.Interfaces;
+using PizzaLab.Services.Data.Models;
+
+namespace PizzaLab.WebApi.Controllers
+{
+    [Route("api/statistics")]
+    [ApiController]
+    public class StatisticsApiController : ControllerBase
+    {
+        private readonly IMenuService menuService;
+
+        public StatisticsApiController(IMenuService menuService)
+        {
+            this.menuService = menuService;
+        }
+
+        [HttpGet]
+        [Produces("application/json")]
+        [ProducesResponseType(200, Type = typeof(StatisticsServiceModel))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> GetStatistics()
+        {
+            try
+            {
+                StatisticsServiceModel serviceModel =
+                    await menuService.GetStatisticsAsync();
+
+                return Ok(serviceModel);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
