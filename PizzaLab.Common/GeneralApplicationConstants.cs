8e4ffd18652a2614e8a392b8ad31eab1562460ad
AleksandarNiko <145496882+AleksandarNiko@users.noreply.github.com>
2024-12-16 21:15:51 +0200

    Fixed current year

--- PizzaLab.Common/GeneralApplicationConstants.cs
+++ PizzaLab.Common/GeneralApplicationConstants.cs
@@ -8,7 +8,7 @@ namespace PizzaLab.Common
 {
     public static  class GeneralApplicationConstants
     {
-        public const int ReleaseYear = 2023;
+        public const int ReleaseYear = 2024;
 
         public const string AdminAreaName = "Admin";
         public const string AdminRoleName = "Administrator";
